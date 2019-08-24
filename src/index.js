71693a026764e765ab7a3e28dc95bc370bede507
ceciliaGeraldo <ceciliandrea.geraldo@gmail.com>
2019-08-24 11:12:19 -0400

    vistas con DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Link, Route, BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Navigation = () =>{
+    return (
+      <section>
+        <App />
+        <ul>
+          <li>
+            <Link to="/vista/a">{'vista A'}</Link>
+          </li>
+          <li>
+            <Link to="/vista/b">{'vista B'}</Link>
+          </li>
+        </ul>
+      </section>
+    );
+  };
+
+  const Vista = ({match}) => {
+    return (
+      <section>
+        <h2>{`Nuestra Vista ${match.params.id}`}</h2>
+      </section>
+    );
+  };
+  
+
+  const AppRouter = () => {
+    return (
+      <Router>
+        <Route component={Navigation} path="/" />
+        <Route component={Vista} path="/vista/:id" />        
+      </Router>
+    );
+  };
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
